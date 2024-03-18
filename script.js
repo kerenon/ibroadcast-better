5efeea5e4e7d07bc9f09566f540147a90315fb14
László Nagy <kerenon@users.noreply.github.com>
2024-03-18 01:12:27 +0100

    Update script.js
    Removed remains of previous attempt to display correct bitrate in player

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const originalSetup = iBroadcastPlay.prototype.setup;
 
 iBroadcastPlay.prototype.setup = function() {
     originalSetup.call(this);
-
-    // Redefine `ongettrackurl` within setup to change the replacement
     this.ongettrackurl = (trackId)=>{
         if (trackId == undefined || trackId == null) {
             return null;
@@ -15,7 +13,6 @@ iBroadcastPlay.prototype.setup = function() {
         }
         if (! cookies.origbitrate && music.user.premium) {
             playurl = playurl.replace(/128/, 320);
-            document.getElementsByClassName('mgr-player-bitrate').innerHTML = '&nbsp;•&nbsp;320Kbps"'
         }
         playurl += "?Expires=" + music.library.expires + "&Signature=" + music.user.token + "&platform=" + ib.client + "&version=1.1&user_id=" + music.user.id + "&file_id=" + trackId;
         ib.log("- play url: " + playurl, this);
